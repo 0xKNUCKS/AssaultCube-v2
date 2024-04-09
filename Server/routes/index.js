80d45926c4aa8d44c3a36e3aef66e7c25ef57a39
Adam A <105402442+adom1337@users.noreply.github.com>
2024-04-09 07:36:37 +0300

    Added POST handler for testing purposes

--- Server/routes/index.js
+++ Server/routes/index.js
@@ -7,4 +7,10 @@ router.get('/', function (req, res) {
     res.render('index', { title: 'Express' });
 });
 
+router.post('/', (req, res) => {
+    const body = JSON.parse(Object.keys(req.body)[0]);
+    console.log("Recieved POST request: ", body);
+    res.send(body)
+})
+
 module.exports = router;
