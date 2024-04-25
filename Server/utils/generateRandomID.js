7f21f3bcce2a28e5f36e119238dde217f478729f
Adam A <105402442+adom1337@users.noreply.github.com>
2024-04-26 00:29:37 +0300

    Server: Renamed the 'seed' to a more proper name for the random ID generator function

--- Server/utils/generateRandomID.js
+++ Server/utils/generateRandomID.js
@@ -1,12 +1,12 @@
 const crypto = require('crypto');
 
-module.exports = (seed, len) => {
+module.exports = (salt, len) => {
   // Create a unique seed with the timestamp
   const timestamp = Date.now().toString();
 
-  // Use the key as an extra seed to generate a hash, honestly useless but why not.
+  // Use the user key as a salt to add additional randomness to the hash, honestly useless but why not.
   const hash = crypto.createHash('sha256');
-  hash.update(seed + timestamp);
+  hash.update(salt || "" + timestamp);
   const result = hash.digest('hex').slice(0, len);
 
   return result;
