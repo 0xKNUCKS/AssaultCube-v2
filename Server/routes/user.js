bc7c650ac4d403d28fdb352740e80611a8eb1313
Adam A <105402442+adom1337@users.noreply.github.com>
2024-05-08 05:47:49 +0300

    Server: Fully implemented a basic sign-up/login system

--- Server/routes/user.js
+++ Server/routes/user.js
@@ -23,7 +23,49 @@ router.post('/register', (req, res) => {
             return res.status(201 /*Created*/).set({
                 'Content-Type': 'application/json',
             }).send({
-                message: "User Registered succesfully!",
+                message: "User Registered succesfully!"
+            })
+        }
+        else {
+            return res.status(409 /*Conflict*/).set({
+                'Content-Type': 'application/json',
+            }).send({
+                message: "Username Already exists! try using another one"
+            })
+        }
+    })
+})
+
+router.post('/login', (req, res) => {
+    const {username, password} = req.body;
+
+    redisClient.hExists("users", username).then((userExists) => {
+        if (userExists) {
+            redisClient.hGet("users", username).then((data) => {
+                let dataObj = JSON.parse(data);
+
+                if (dataObj.hasOwnProperty("password")) {
+                    if (password === dataObj["password"]) {
+                        return res.status(200 /*OK*/).set({
+                            'Content-Type': 'application/json'
+                        }).send({
+                            message: "Success!"
+                        })
+                    } else { // hate the repetitive code, will be improved later
+                        return res.status(404 /*Not Found*/).set({
+                            'Content-Type': 'application/json'
+                        }).send({
+                            message: "Username or password is invalid!"
+                        })
+                    }
+                }
+            })
+        }
+        else {
+            return res.status(404 /*Not Found*/).set({
+                'Content-Type': 'application/json'
+            }).send({
+                message: "Username or password is invalid!" // i dont know why websites always mislead with the "incorrect username OR password", but ill do it too. :D
             })
         }
     })
