d9e0afc78777404324d136fd84566ab0589f955c
Adam A <105402442+adom1337@users.noreply.github.com>
2024-06-15 18:00:49 +0300

    Added a token system & improved some code's readability

--- Server/routes/user.js
+++ Server/routes/user.js
@@ -2,7 +2,8 @@
 const express = require('express');
 const router = express.Router();
 const redisClient = require("../shared/redisClient")
-const bcrypt = require("bcrypt")
+const bcrypt = require("bcrypt");
+const generateRandomID = require('../utils/generateRandomID');
 
 router.get('/', (req, res) => {
     // Give back user info to be then displayed in a "profile" page, not much to give now tho
@@ -15,38 +16,38 @@ router.post('/register', (req, res) => {
         return res.status(400 /*Bad Request*/).set({
             'Content-Type': 'application/json',
         }).send({
-            message: "Username and password are required!"
+            message: "Username and password are required to register!"
         })
     }
 
     const timeStamp = new Date().toISOString();
 
     redisClient.hExists("users", username).then((userExists) => {
-        if (!userExists) {
-            bcrypt.hash(password, 11).then((hashedPassword) => {
-                redisClient.hSet("users", username, JSON.stringify({
-                    password: hashedPassword,
-                    ip: req.ip,
-                    timestamps: {
-                        created: timeStamp,
-                        last_login: timeStamp
-                    }
-                })).then(() => {console.log(`> Registered New user (${username}:${req.ip})`)})
-            })
-
-            return res.status(201 /*Created*/).set({
-                'Content-Type': 'application/json',
-            }).send({
-                message: "User Registered succesfully!"
-            })
-        }
-        else {
+        if (userExists) {
             return res.status(409 /*Conflict*/).set({
                 'Content-Type': 'application/json',
             }).send({
                 message: "Username Already exists! try using another one"
             })
         }
+
+        bcrypt.hash(password, 11).then((hashedPassword) => {
+            redisClient.hSet("users", username, JSON.stringify({
+                password: hashedPassword,
+                ip: req.ip,
+                token: null,
+                timestamps: {
+                    created: timeStamp,
+                    last_login: timeStamp
+                }
+            })).then(() => {console.log(`> Registered New user (${username}:${req.ip})`)})
+        })
+
+        return res.status(201 /*Created*/).set({
+            'Content-Type': 'application/json',
+        }).send({
+            message: "User Registered succesfully!"
+        })
     })
 })
 
@@ -57,47 +58,66 @@ router.post('/login', (req, res) => {
         return res.status(400 /*Bad Request*/).set({
             'Content-Type': 'application/json',
         }).send({
-            message: "Username and password are required!"
+            message: "Username and password are required!",
+            data: {
+                token: null
+            }
         })
     }
     
-    const timeStamp = new Date().toISOString();
+    const timeStamp = new Date();
 
     redisClient.hExists("users", username).then((userExists) => {
-        if (userExists) {
-            redisClient.hGet("users", username).then((data) => {
-                let dataObj = JSON.parse(data);
-
-                // Update the last login timestamp
-                dataObj["timestamps"]["last_login"] = timeStamp
-                redisClient.hSet("users", username, JSON.stringify(dataObj)).then(() => {/* log or something lol */})
-
-                if (dataObj.hasOwnProperty("password")) {
-                    bcrypt.compare(password, dataObj["password"]).then((result) => {
-                        if (result) {
-                            return res.status(200 /*OK*/).set({
-                                'Content-Type': 'application/json'
-                            }).send({
-                                message: "Success!"
-                            })
-                        } else { // hate the repetitive code, will be improved later
-                            return res.status(404 /*Not Found*/).set({
-                                'Content-Type': 'application/json'
-                            }).send({
-                                message: "Username or password is invalid!"
-                            })
-                        }
-                    })
-                }
-            })
-        }
-        else {
+        if (!userExists) {
             return res.status(404 /*Not Found*/).set({
                 'Content-Type': 'application/json'
             }).send({
                 message: "Username or password is invalid!" // i dont know why websites always mislead with the "incorrect username OR password", but ill do it too. :D
             })
         }
+
+        redisClient.hGet("users", username).then((data) => {
+            let userData = JSON.parse(data);
+
+            if (userData.hasOwnProperty("password")) {
+                bcrypt.compare(password, userData["password"]).then((result) => {
+                    if (result) { // passed!
+                        // generate a temporary token valid for 24hrs used to validate actions in the API.
+                        const tokenId = generateRandomID(req.ip + username, 32)
+                        const tokenExpiryDate = new Date(new Date().setDate(timeStamp.getDate() + 1)) // 1 day from now.
+                        
+                        // Update the last login timestamp & store token
+                        userData["timestamps"]["last_login"] = timeStamp.toISOString()
+                        userData["token"] = {
+                            key: tokenId,
+                            expiry: tokenExpiryDate
+                        }
+
+                        redisClient.hSet("users", username, JSON.stringify(userData)).then(() => {
+                            console.log(`> User successfully logged in [${username} with token ${tokenId}]`)
+                        })
+
+                        return res.status(200 /*OK*/).set({
+                            'Content-Type': 'application/json'
+                        }).send({
+                            message: "Success!",
+                            data: {
+                                token: tokenId
+                            }
+                        })
+                    } else { // hate the repetitive code, will be improved later
+                        return res.status(404 /*Not Found*/).set({
+                            'Content-Type': 'application/json'
+                        }).send({
+                            message: "Username or password is invalid!",
+                            data: {
+                                token: null
+                            }
+                        })
+                    }
+                })
+            }
+        })
     })
 })
 
